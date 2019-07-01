ab27b76e349ff2b077ef99ec99f85d3ee582d09d
Andrew <siricoa94@github.com>
2019-07-01 12:53:35 -0400

    Got red gem to only display one number and not generate new number on each push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,24 +5,29 @@ var redCrystal = 0;
 var blueCrystal = 0;
 var greenCrystal = 0;
 var yellowCrystal = 0;
-var operatorSelected = false;
+
 win = undefined;
 loss = undefined;
+min = 12;
+max = 120;
 
 $(document).ready(function(){
+    var randomNumber = Math.floor(Math.random() * 102) + 19;
+    $("#rngGenerator").text("The number to match is: " + randomNumber);
+    var redCrystal = Math.floor(Math.random()*12)
     
-    $("#rngGenerator").ready(function() {
-        var randomNumber = Math.floor(Math.random() * 100) + 12;
-        $("#rngGenerator").text("The number to match is: " + randomNumber);
-        
-        console.log(randomNumber)
+   
 
-    });
-    $('#redCrystal').on("click", function(){
-        operatorSelected = true;
-        var redCrystal = Math.floor(Math.random()*12)
+    
+    $("#redCrystal").on("click", function(){
+        
         $('#totalScoreDisplay').text(redCrystal);
         
       })
+      $("#blueCrystal").on("click", function(){
+        var blueCrystal = Math.floor(Math.random()*12)
+        $('#totalScoreDisplay').text(blueCrystal);
+        
+      })
     
-})
+});
